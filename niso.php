214ec2b2ad0f281f9a0a2c40fb5be83d1454fa91
Nino <ninoemery12@gmail.com>
2018-05-14 09:22:42 +0200

    Update and rename niso.html to niso.php

--- niso.php
+++ niso.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-  <p style="font-size:40px">Random Stats :</p><\br>
+  <p style="font-size:40px">Random Stats :</p></br>
   <p style="font-size:30px">O!Std : 
   <?php 
   $url = 'http://old.ppy.sh/api/get_user?u=N1sO&m=0&k=b9cc04896a054a42d63ac8648de26b8ea93ddf1b';
